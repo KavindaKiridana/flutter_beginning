6f3a12a6d1064dc6387a2fbc533b95c97d6ed36d
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-09-23 10:45:59 +0530

    Update widget_tree.dart

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter5/comming_from_internet.dart';
 import 'package:flutter5/internet_page.dart';
 import 'package:flutter5/notifier.dart';
 import 'package:flutter5/profile.dart';
 import 'package:flutter5/about.dart';
 import 'package:flutter5/top_a_bar.dart';
 
-List<Widget> pages = [About(), Profile(), InternetPage()];
+List<Widget> pages = [
+  About(),
+  Profile(),
+  InternetPage(),
+  CommingFromInternet(),
+];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -27,6 +33,10 @@ class WidgetTree extends StatelessWidget {
               NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
               NavigationDestination(
                 icon: Icon(Icons.integration_instructions_rounded),
+                label: 'internet',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.signal_wifi_connected_no_internet_4),
                 label: 'internet',
               ),
             ],
