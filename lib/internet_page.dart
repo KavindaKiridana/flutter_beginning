6804b2b4a3607e965524e5417048cd7653c13a19
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-09-23 09:04:30 +0530

    Update internet_page.dart

--- lib/internet_page.dart
+++ lib/internet_page.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class InternetPage extends StatefulWidget {
   const InternetPage({super.key});
@@ -8,6 +12,28 @@ class InternetPage extends StatefulWidget {
 }
 
 class _InternetPageState extends State<InternetPage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.https('v2.jokeapi.dev', '/joke/Any');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['delivery'];
+      print('internet page');
+      log('Number of books about http: $itemCount.');
+      log(response.body);
+    } else {
+      log('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
